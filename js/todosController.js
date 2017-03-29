16ce3d846e203549adb149c42806eb10cb570ad4
Elizabeth Donohue <elizabeth.r.donohue@gmail.com>
2017-03-29 12:06:37 -0700

    adds seed todos

--- js/todosController.js
+++ js/todosController.js
@@ -2,5 +2,20 @@ angular.module('todoApp')
   .controller(TodosController, TodosController)
 
 function TodosController() {
+  var self = this
 
+  self.allTodos = [
+    {
+      content: "complete this lab",
+      completed: false
+    },
+    {
+      content: "workout",
+      completed: false
+    },
+    {
+      content: "make dinner",
+      completed: true
+    }
+  ]
 }
